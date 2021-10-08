2facb72197efb9b03b547a77808f66ea1dc56fa3
yeshuai <yeshuai@divtoss.com>
2021-10-08 21:16:55 +0800

    feat: 格式化手机号

---
+++ 0008-格式化手机号/index.js
@@ -0,0 +1,28 @@
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) // => returns "(111) 111-1111"
+
+// 解法一
+function createPhoneNumber1(numbers){
+  var format = "(xxx) xxx-xxxx";
+  for(var i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i]);
+  }
+  return format;
+}
+
+// 解法二
+function createPhoneNumber2(numbers){
+  const format = "(xxx) xxx-xxxx";
+  const loop = (cur, next) => cur.replace('x',next)
+  return numbers.reduce(loop, format)
+}
+
+// 解法三
+function createPhoneNumber3(numbers){
+  return numbers.join('').replace(/(...)(...)(.*)/,'($1) $2-$3')
+}
+
+console.log(`createPhoneNumber1`, createPhoneNumber1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+console.log(`createPhoneNumber2`, createPhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+console.log(`createPhoneNumber3`, createPhoneNumber3([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
